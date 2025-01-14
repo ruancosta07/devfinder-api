39c147dacefe7cf06053f3d918a0c05918189e0d
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-01-14 14:49:17 -0300

    config do servidor

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import userRoutes from "./routes/user";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json"
+const app = express();
+
+app.use(express.json());
+app.use(userRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {explorer: true}));
+app.listen(3000, () => {
+  try {
+    console.log(`Server is running at 3000 port`);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+export { app };
