7f513284b508ac9e010d0e54d921ac370cc6067a
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-01-14 14:51:03 -0300

    rotas do usuário

---
+++ src/routes/user.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import {
+  confirmCode,
+  createUser,
+  deleteAccount,
+  getProjects,
+  login,
+  uploadProject,
+  verifyToken
+} from "../controllers/User";
+import upload from "../utils/multer";
+import { authMiddleware } from "../middlewares/authMiddleware.ts";
+const userRoutes = Router()
+
+userRoutes.post("/criar-conta", createUser)
+userRoutes.post("/login", login)
+userRoutes.post("/:userId/criar-projeto", authMiddleware, upload.array("images"),  uploadProject)
+userRoutes.post("/confirmar-codigo",  confirmCode)
+userRoutes.post("/verificar-conta", verifyToken);
+userRoutes.get("/:id/carregar-projetos", authMiddleware, getProjects);
+userRoutes.delete("/:id/excluir-conta",authMiddleware, deleteAccount);
+
+export default userRoutes
