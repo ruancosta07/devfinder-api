8cc669e2adb70c4423eaf012d18382c883a392fb
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-01-14 14:50:36 -0300

    middlewares de autenticação

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,97 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import prisma from "../utils/prisma";
+const jwtKey = process.env.JWT_KEY as string;
+
+type Token = {
+  name: string;
+  email: string;
+  role: string;
+};
+
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      res.status(401).json({
+        message: "Usuário(a) não possui permissão para realizar essa ação",
+      });
+      return;
+    }
+    const [, token] = authorization.split(" ") as string[];
+    if (!token) {
+      res.status(401).json({
+        message: "Usuário(a) não possui permissão para realizar essa ação",
+      });
+      return;
+    }
+    const decodedToken = jwt.verify(token, jwtKey) as Token;
+    const userExists = await prisma.users.findFirst({
+      where: {
+        email: decodedToken.email,
+      },
+    });
+    if (!userExists) {
+      res.status(401).json({
+        message: "Usuário(a) não possui permissão para realizar essa ação",
+      });
+      return;
+    }
+    next();
+  } catch (err) {
+    res
+      .status(401)
+      .json({
+        message: "Usuário(a) não possui permissão para realizar essa ação",
+      });
+    return;
+  }
+};
+const recruiterMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      res.status(401).json({
+        message: "Usuário(a) não possui permissão para realizar essa ação",
+      });
+      return;
+    }
+    const [, token] = authorization.split(" ") as string[];
+    if (!token) {
+      res.status(401).json({
+        message: "Usuário(a) não possui permissão para realizar essa ação",
+      });
+      return;
+    }
+    const decodedToken = jwt.verify(token, jwtKey) as Token;
+    const userExists = await prisma.users.findFirst({
+      where: {
+        email: decodedToken.email,
+      },
+    });
+    if (!userExists || userExists.role !== "Recrutador") {
+      res.status(401).json({
+        message: "Usuário(a) não possui permissão para realizar essa ação",
+      });
+      return;
+    }
+    next();
+  } catch (err) {
+    res
+      .status(401)
+      .json({
+        message: "Usuário(a) não possui permissão para realizar essa ação",
+      });
+    return;
+  }
+};
+
+export {authMiddleware, recruiterMiddleware}
